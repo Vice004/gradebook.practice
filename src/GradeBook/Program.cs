cf6f536448eda84461c400501a1ab148812fd03c
Molefi <Molefi.Ngakane@Britehouse.co.za>
2020-02-24 10:30:37 +0200

    mods

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -6,6 +6,9 @@ namespace GradeBook
     {
         static void Main(string[] args)
         {
+            var numbers = new double[3];
+            //numbers[0] = 34.4;
+
             if(args.Length > 0){
                 Console.WriteLine($"Hello, {args[0]} !");
             }else{
