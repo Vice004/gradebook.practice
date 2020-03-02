fab31bf7d8eeb6a8e05da71d818f786c2c0f9c64
Molefi <Molefi.Ngakane@Britehouse.co.za>
2020-03-02 10:01:28 +0200

    ecapsulated object

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -6,25 +6,14 @@ namespace GradeBook
     class Program
     {
         static void Main(string[] args)
-        {
-            Book book2 = new Book();
-            book2.AddGrade(90.1);
+        { 
 
-            var book = new Book();
+            var book = new Book("Molefi's Grade Book");
             book.AddGrade(89.1);
+            book.AddGrade(90.5);
+            book.AddGrade(77.5);
+            book.showStatistics();
 
-            //creating a list of grades
-            var grades = new List<double>() { 12.7, 10.3, 6.11, 4.1 };
-            grades.Add(56.1);
-
-            var result = 0.0;
-            foreach(double number in grades)
-            {
-                result += number;
-            }
-            result /= grades.Count;
-            Console.WriteLine($"The average grade is {result:N1}");
-            
         }
     }
 }
