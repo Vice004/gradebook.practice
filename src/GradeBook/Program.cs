09fca11319399262a210640d16f0d855c891110d
Molefi <Molefi.Ngakane@Britehouse.co.za>
2020-03-02 14:09:33 +0200

    added unit tests

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -12,7 +12,13 @@ namespace GradeBook
             book.AddGrade(89.1);
             book.AddGrade(90.5);
             book.AddGrade(77.5);
-            book.showStatistics();
+            book.AddGrade(69.6);
+            book.AddGrade(72.8);
+
+            var stats = book.GetStatistics();
+            Console.WriteLine($"The lowest grade is {stats.Low}");
+            Console.WriteLine($"The highest grade is {stats.High}");
+            Console.WriteLine($"The average grade is {stats.Average}");
 
         }
     }
